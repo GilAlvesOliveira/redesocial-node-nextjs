3fdb4e84a0f7b2f96451b4e1dba5345207547df7
Ana Cristina oliveira <121919367+GilAlvesOliveira@users.noreply.github.com>
2023-08-25 15:30:56 -0300

    ...

---
+++ models/UsuarioModel.ts
@@ -0,0 +1,14 @@
+import mongoose, { Schema } from 'mongoose';
+
+const UsuarioSchema = new Schema({
+    nome: {type: String, required: true},
+    email: {type: String, required: true},
+    senha: {type: String, required: true},
+    avatar: {type: String, required: false},
+    seguidores: {type: Number, default: 0},
+    seguindo: {type: Number, default: 0},
+    publicacoes: {type: Number, default: 0}
+});
+
+export const UsuarioModel = (mongoose.models.usuarios || mongoose.model('usuarios', UsuarioSchema)); 
+//exporta a tabela onde o mogoose dentro das tabelas dela esta criado..se tiver criado ou se não esistir ele cria uma tabela
