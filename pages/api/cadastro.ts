f6bba8d7eff12812c6a390369f30359d5588eacb
Ana Cristina oliveira <121919367+GilAlvesOliveira@users.noreply.github.com>
2023-08-25 15:44:15 -0300

    Update cadastro.ts

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -1,19 +1,18 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
 import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
 import type { cadastroRequisicao } from '../../types/CadastroRequisicao';
 import { UsuarioModel } from '../../models/UsuarioModel';
-import { conectarMongoDB } from '../../middlewares/conectarMongoDB'
-
 
 const endpointCadastro = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
+
     if(req.method === 'POST') {
         const usuario = req.body as cadastroRequisicao;
 
         if(!usuario.nome || usuario.nome.length < 2) {
-            return res.status(400).json({erro: 'Nome invalido'})
+            return res.status(400).json({erro: 'Nome invalido'});
         }
 
-        if(!usuario.email || usuario.email.length < 5 
+        if(!usuario.email || usuario.email.length < 5
             || !usuario.email.includes('@')
             || !usuario.email.includes('.')) {
             return res.status(400).json({erro: 'Email invalido'});
@@ -22,12 +21,10 @@ const endpointCadastro = async (req: NextApiRequest, res: NextApiResponse<Respos
         if(!usuario.senha || usuario.senha.length < 4) {
             return res.status(400).json({erro: 'Senha invalida'});
         }
-
         await UsuarioModel.create(usuario);
-
         return res.status(200).json({msg: 'Usuario criado com sucesso'});
     }
-    return res.status(405).json({erro: 'Metodo informado não é valido'});
+    return res.status(405).json({erro: 'Metodo informado nao e valido'});
 }
 
 export default endpointCadastro;
