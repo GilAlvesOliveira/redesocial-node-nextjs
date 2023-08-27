39a2e129de969d8b95b18409042f0c44f365efbd
Ana Cristina oliveira <121919367+GilAlvesOliveira@users.noreply.github.com>
2023-08-27 20:10:40 -0300

    Schemas parte 2 concluida

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -2,29 +2,42 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
 import type { cadastroRequisicao } from '../../types/CadastroRequisicao';
 import { UsuarioModel } from '../../models/UsuarioModel';
+import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
+import md5 from "md5";
 
 const endpointCadastro = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
 
-    if(req.method === 'POST') {
+    if(req.method === 'POST') {                          //aqui verifica o metodo que seria POST
         const usuario = req.body as cadastroRequisicao;
 
-        if(!usuario.nome || usuario.nome.length < 2) {
+        if(!usuario.nome || usuario.nome.length < 2) {   // aqui verifica se o nome tem menos de 2 letras..se tiver ele retorna um erro
             return res.status(400).json({erro: 'Nome invalido'});
         }
 
-        if(!usuario.email || usuario.email.length < 5
-            || !usuario.email.includes('@')
+        if(!usuario.email || usuario.email.length < 5    // aqui verifica se tem um email e se ele tem mais de 5 caracteres e tbm se tem . e @ 
+            || !usuario.email.includes('@')           
             || !usuario.email.includes('.')) {
             return res.status(400).json({erro: 'Email invalido'});
         }
 
-        if(!usuario.senha || usuario.senha.length < 4) {
+        if(!usuario.senha || usuario.senha.length < 4) { // aqui verifica se a senha tem mais de 4 caracteres
             return res.status(400).json({erro: 'Senha invalida'});
         }
-        await UsuarioModel.create(usuario);
+
+        const usuarioComMesmoEmail = await UsuarioModel.find({email: usuario.email}); // aqui seria uma variavel para verificar se ja exixte um usuario com mesma email
+        if(usuarioComMesmoEmail && usuarioComMesmoEmail.length > 0) {
+            return res.status(400).json({erro: 'Ja existe uma conta com o email informado'});
+        }
+        
+        const usuarioASerSalvo = {                        // aqui cria uma variavel que tenha nome, email, senha e vai criptografar a senha 
+            nome: usuario.nome,
+            email: usuario.email,
+            senha: md5(usuario.senha)
+        }
+        await UsuarioModel.create(usuarioASerSalvo);
         return res.status(200).json({msg: 'Usuario criado com sucesso'});
     }
     return res.status(405).json({erro: 'Metodo informado nao e valido'});
 }
 
-export default endpointCadastro;
+export default conectarMongoDB(endpointCadastro);
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,17 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
 import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
+import md5 from 'md5';
+import { UsuarioModel } from '../../models/UsuarioModel';
 
-const endpointLogin = (                      //esta linha era... export default (
+const endpointLogin = async (                      //esta linha era... export default (
     req: NextApiRequest,
     res: NextApiResponse<RespostaPadraoMsg>   //dentro do <> coloca o type importado para padronizar as respostas
 ) => {
     if(req.method === 'POST') { //aqui esta validando o metodo
         const {login, senha} = req.body;
 
-        if(login === 'admin@admin.com' && //este login e senha esta mocado apenas para fazer os testes.
-        senha === "Admin@123"){
-            return res.status(200).json({msg: 'Usuario autenticado com sucesso'}); // aqui seria as msg de ero ou status
+        const usuariosEncontrados = await UsuarioModel.find({email: login, senha: md5(senha)});
+       // if(login === 'admin@admin.com' &&  senha === "Admin@123"){                // este login e senha esta mocado apenas para fazer os testes.
+        if(usuariosEncontrados && usuariosEncontrados.length > 0) {                 //verifica se tem usuario e pega o primeiro
+            const usuarioEncontrado = usuariosEncontrados[0];                           // aqui usuarioEncontrado vai ser o usuariosncontrados
+            return res.status(200).json({msg: `Usuario ${usuarioEncontrado.nome} autenticado com sucesso`}); // aqui seria as msg de erro ou status
         }
         return res.status(400).json({erro: 'Usuario ou senha não encontrado'});
     }
