aab71c969b4e7ea1d5831127a119351bc15a92a3
Ana Cristina oliveira <121919367+GilAlvesOliveira@users.noreply.github.com>
2023-08-28 14:26:08 -0300

    jwt e autenticação terminada

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,13 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
 import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
+import type { LoginResposta } from '../../types/LoginResposta';
 import md5 from 'md5';
 import { UsuarioModel } from '../../models/UsuarioModel';
+import jwt from 'jsonwebtoken';
 
 const endpointLogin = async (                      //esta linha era... export default (
     req: NextApiRequest,
-    res: NextApiResponse<RespostaPadraoMsg>   //dentro do <> coloca o type importado para padronizar as respostas
+    res: NextApiResponse<RespostaPadraoMsg | LoginResposta>   //dentro do <> coloca o type importado para padronizar as respostas
 ) => {
+
+    const {MINHA_CHAVE_JWT} = process.env;
+    if(!MINHA_CHAVE_JWT) {
+        return res.status(500).json({erro: 'ENV Jwt não informada.'});
+    }
+
     if(req.method === 'POST') { //aqui esta validando o metodo
         const {login, senha} = req.body;
 
@@ -15,7 +23,10 @@ const endpointLogin = async (                      //esta linha era... export de
        // if(login === 'admin@admin.com' &&  senha === "Admin@123"){                // este login e senha esta mocado apenas para fazer os testes.
         if(usuariosEncontrados && usuariosEncontrados.length > 0) {                 //verifica se tem usuario e pega o primeiro
             const usuarioEncontrado = usuariosEncontrados[0];                           // aqui usuarioEncontrado vai ser o usuariosncontrados
-            return res.status(200).json({msg: `Usuario ${usuarioEncontrado.nome} autenticado com sucesso`}); // aqui seria as msg de erro ou status
+            
+            const token = jwt.sign({_id: usuarioEncontrado._id}, MINHA_CHAVE_JWT);
+            
+            return res.status(200).json({nome: usuarioEncontrado.nome, email: usuarioEncontrado.email, token}); // aqui seria as msg de erro ou status
         }
         return res.status(400).json({erro: 'Usuario ou senha não encontrado'});
     }
