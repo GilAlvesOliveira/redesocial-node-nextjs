167232725e8cc2cc4ebf27b9e3cb7cfb71e3d3b1
Ana Cristina oliveira <121919367+GilAlvesOliveira@users.noreply.github.com>
2023-09-07 21:53:09 -0300

    Update feed.ts

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -7,17 +7,18 @@ import { PublicacaoModel } from "@/models/PublicacaoModel";
 
 const feedEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any>) => {
     try {
-        if(req.method === 'GET') {
-            if(req?.query?.id) {
-                const usuario = await UsuarioModel.findById(req?.query?.id);
+        if(req?.method === 'GET') {
+            if(req?.query?.userId) {
+                const usuario = await UsuarioModel.findById(req?.query?.userId);
                 if(!usuario) {
                     return res.status(400).json({erro: 'Usuario não encontrado.'});
                 }
                 const publicacoes = await PublicacaoModel.find({idUsuario: usuario._id}).sort({data: -1});
                 return res.status(200).json(publicacoes);
             }
+        } else {
+            return res.status(405).json({erro: 'Metodo informado não é valido.'});
         }
-        return res.status(405).json({erro: 'Metodo informado não é valido.'});
     } catch(e) {
         console.log(e);
         return res.status(400).json({erro: "Não foi possivel obter o feed "});
