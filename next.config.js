9ee523112a376ee109b4dd36c89e3164b4c46270
Ana Cristina oliveira <121919367+GilAlvesOliveira@users.noreply.github.com>
2023-09-28 14:49:38 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  /* typescript: {
+    ibnoreBuildErrors: true        ....aqui seria para elimiar o erro quando for sunbir apra o deploy 
+   } */
 }
 
 module.exports = nextConfig
