d680dc3028f830012081ccc4970b796d03272d20
Ana Cristina oliveira <121919367+GilAlvesOliveira@users.noreply.github.com>
2023-09-28 14:00:17 -0300

    Update politicaCORS.ts

--- middlewares/politicaCORS.ts
+++ middlewares/politicaCORS.ts
@@ -2,19 +2,18 @@ import type { NextApiRequest, NextApiResponse, NextApiHandler } from "next";
 import type { RespostaPadraoMsg } from "@/types/RespostaPadraoMsg";
 import NextCors from "nextjs-cors";
 
-export const politicaCORS = (handler: NextApiHandler) => {
+export const politicaCORS = (handler: NextApiHandler) =>
     async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
         try {
             await NextCors(req, res, {
-                origem: '*',
-                method: ['GET', 'POST', 'PUT', 'DELETE' ],
-                optionsSuccessStatus: 200,
+                origin: '*',
+                methods: ['GET', 'POST', 'PUT', 'DELETE'],
+                optionsSuccessStatus:200,
             });
 
-            return handler(req, res);
-        } catch(e){
-            console.log('Erro ao tratar politica de CORS', e);
-            return res.status(500).json({erro: 'Ocorreu erro ao tratar a lolitica de CORS.'});
-        }
+        return handler(req, res);
+    } catch(e) {
+        console.log('Erro ao tratar logica de politica de CORS', e);
+        return res.status(500).json({erro : 'Ocorreu erro ao tratar politica de CORS'});
     }
 }
